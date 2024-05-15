52fb73d474763266484e75a2dd5f60a333e1c862
GirasArya <girasarya130@gmail.com>
2024-05-15 14:33:11 +0700

    Implement layout for login activity

---
+++ app/src/main/java/com/capstone/dressify/LoginActivity.kt
@@ -0,0 +1,34 @@
+package com.capstone.dressify
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.capstone.dressify.databinding.ActivityLoginBinding
+import androidx.appcompat.widget.Toolbar
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //Toolbar
+        setSupportActionBar(findViewById(R.id.tb_login))
+        setTitle(null)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back_arrow)
+        supportActionBar?.elevation = 0f
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.ivGoogleLogin.setOnClickListener {
+            Toast.makeText(this, "Fitur belum selesai dibuat", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
