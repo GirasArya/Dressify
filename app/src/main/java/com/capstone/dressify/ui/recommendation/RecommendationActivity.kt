f4771307c9e1957ad8107c86d476fecbe0b1feb9
Genthur <genthuraryoseto510@gmail.com>
2024-05-17 13:33:04 +0700

    Adding layout for Recommendation features.

---
+++ app/src/main/java/com/capstone/dressify/ui/recommendation/RecommendationActivity.kt
@@ -0,0 +1,21 @@
+package com.capstone.dressify.ui.recommendation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.capstone.dressify.R
+
+class RecommendationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_recommendation)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
