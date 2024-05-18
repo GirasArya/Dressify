555a0b124bd292ae0609d5101739e24e94d73ab3
GirasArya <girasarya130@gmail.com>
2024-05-18 20:40:58 +0700

    Added login and logout with firebase

--- app/src/main/java/com/capstone/dressify/ui/main/ProfileFragment.kt
+++ app/src/main/java/com/capstone/dressify/ui/main/ProfileFragment.kt
@@ -7,36 +7,68 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.capstone.dressify.R
 import com.capstone.dressify.databinding.FragmentProfileBinding
 import com.capstone.dressify.ui.landing.LandingActivity
+import com.capstone.dressify.ui.login.LoginActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import kotlinx.coroutines.launch
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentProfileBinding.inflate(inflater, container, false)
 
         binding.btnLogout.setOnClickListener {
-            val intent = Intent(this@ProfileFragment.requireContext(), LandingActivity::class.java)
-            startActivity(intent)
+            signOut()
         }
 
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        auth = Firebase.auth // Initialize auth here
+        val firebaseUser = auth.currentUser
+
+        if (firebaseUser != null) {
+            val email = firebaseUser.email
+            val displayName = firebaseUser.displayName
+            val profileUser = firebaseUser.photoUrl
+
+
+            Glide.with(this)
+                .load(profileUser)
+                .placeholder(R.drawable.ic_avatar_profile)
+                .circleCrop()
+                .into(binding.ivAvatarProfile)
+
+            binding.tvProfileEmail.text = email ?: "Email not available"
+            binding.tvProfileUsername.text = displayName ?: "Username not available"
+        }
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        startActivity(Intent(this@ProfileFragment.requireContext(), LandingActivity::class.java))
     }
 }
