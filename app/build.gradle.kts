b38df166fba761363fef942b35261ddbb8bfa9a8
Genthur <genthuraryoseto510@gmail.com>
2024-06-10 18:14:50 +0700

    Implement bounding box to shoulder from tflite model

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,8 +85,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-tflite-gpu:16.2.0")
     implementation("org.tensorflow:tensorflow-lite-task-vision-play-services:0.4.2")
     implementation("com.google.android.gms:play-services-tflite-support:16.1.0")
-    implementation(libs.firebase.firestore.ktx)
-    implementation(libs.play.services.vision.common)
 
     ksp("androidx.room:room-compiler:2.6.1")
 
@@ -114,8 +112,6 @@ dependencies {
     // Viewpager and dots indicator
     implementation("me.relex:circleindicator:2.1.6")
 
-    //open cv
-    implementation(project(":sdk"))
 
     //camera
     implementation("androidx.camera:camera-core:1.1.0")
@@ -138,4 +134,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-}
+}}
---
+++ app/src/main/java/com/capstone/dressify/ui/view/camera/ModelObjects.kt
@@ -0,0 +1,6 @@
+package com.capstone.dressify.ui.view.camera
+
+object ModelObjects {
+    const val MODEL_PATH = "new_ar.tflite"
+    const val LABELS_PATH = "labels.txt"
+}
